586419e97a6bb60fc347ca53daf33d0f831e0c9b
yellhtetaung <yairhtet248.yha@gmail.com>
2021-10-14 11:10:55 +0630

    updated

--- script.js
+++ script.js
@@ -15,3 +15,27 @@ parentContainer.addEventListener('click', event=>{
     current.textContent = current.textContent.includes('Read More') ? "Read Less..." : "Read More...";
 
 })
+// music by sogy
+const audioTag = document.querySelector("audio");
+const playBut = document.querySelector(".play");
+const pauseBut = document.querySelector(".pause");
+
+playBut.addEventListener("click", () => {
+    audioTag.play();
+    
+    playBut.style.zIndex = "-1";
+    pauseBut.style.zIndex = "0";
+
+    playBut.style.transform = "translate(-50%, -50%) rotate(90deg)";
+    pauseBut.style.transform = "translate(-50%, -50%) rotate(0deg)";
+})
+
+pauseBut.addEventListener("click", () => {
+    audioTag.pause();
+   
+    playBut.style.zIndex = "0";
+    pauseBut.style.zIndex = "-1";
+
+    playBut.style.transform = "translate(-50%, -50%) rotate(0deg)";
+    pauseBut.style.transform = "translate(-50%, -50%) rotate(-90deg)";
+})
